4cb0704bd4f1b1d5c3a763a478d40bc21ebdfafa
Nayeem Laheji <132880575+nayeem003@users.noreply.github.com>
2024-06-23 22:39:13 +0530

    Add files via upload

---
+++ array_cancellation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n+1];
+        for (int i = 1; i<=n; i++) cin >> arr[i];
+
+        long long pos = 0;
+        long long rem_neg = 0;
+        for (int i = 1; i<=n; i++)
+        {
+            if (arr[i] >=0)
+            {
+                pos +=arr[i];
+            }
+            else
+            {
+                if (pos >= abs(arr[i]))
+                {
+                    pos -= abs(arr[i]);
+                }
+                else{
+                    rem_neg +=(abs(arr[i])-pos);
+                    pos = 0;
+                }
+            }
+        }
+
+        cout << rem_neg <<endl;
+
+
+    }
+
+}
