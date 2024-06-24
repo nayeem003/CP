69c929f6cff44f3fcc5a352303802741b4cc278c
Nayeem Laheji <132880575+nayeem003@users.noreply.github.com>
2024-06-24 21:28:39 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+
+    cout <<n/2 << endl;
+
+}
+int main(){
+    io();
+    int t;
+    cin >> t;
+
+    for(int i=1; i<=t ; i++) solve();
+
+    return 0;
+}
